e93110bbf6069866d07526017f4ae29a6e577455
Steven <stevenbarnes50@gmail.com>
2026-01-05 14:41:55 -0700

    Enable Postgres + fix sms_opt_in_migration

--- Football_Project/__init__.py
+++ Football_Project/__init__.py
@@ -77,8 +77,13 @@ def odds_escalation_job_with_context(app):
 def create_app():
     app = Flask(__name__)
 
+    db_url = os.getenv("DATABASE_URL", "sqlite:///picks.db")
+    if db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+
     # Config
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///picks.db")
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url
+    #app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///picks.db")
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "password")
     app.config["WTF_CSRF_SECRET_KEY"] = os.getenv("WTF_CSRF_SECRET_KEY", "csrf-key-value")
 
