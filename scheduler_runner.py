d7b31685bcda39e4582a3df832aa1e368ed67d6f
Ansible User <stevenbarnes50@gmail.com>
2026-04-27 17:26:20 +0000

    update scheduler_runner.py

--- scheduler_runner.py
+++ scheduler_runner.py
@@ -2,7 +2,7 @@ import time
 from wsgi import app
 
 # Importing app triggers scheduler startup when DISABLE_APSCHEDULER is not "1"
-
+print("[SCHEDULER] STARTED scheduler_runner.py", flush=True)
 if __name__ == "__main__":
     try:
         while True:
