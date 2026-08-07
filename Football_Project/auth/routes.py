2377b17b4a64a75c8782531bce037cdb0151b24b
Steven <stevenbarnes50@gmail.com>
2026-08-07 07:45:36 -0600

    adding print result to get better logs of login

--- Football_Project/auth/routes.py
+++ Football_Project/auth/routes.py
@@ -97,7 +97,8 @@ def login():
                 user.set_password(pwd)
                 db.session.commit()
 
-            login_user(user, remember=form.remember.data, force=True)
+            result = login_user(user, remember=form.remember.data, force=True)
+            print(f"[LOGIN] result={result} is_active={user.is_active} authenticated={current_user.is_authenticated}")
 
             pending_token = session.pop("pending_invite_token", None)
             if pending_token:
